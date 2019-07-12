147a4d175c1f6afd10855af4f52f536a7bc2ad02
George Cui <georgeluocui@gmail.com>
2019-07-13 00:36:14 +0800

    key-value

--- xml.py
+++ xml.py
@@ -1,27 +1,40 @@
 # -*- coding:utf-8 -*-
 from lxml import etree
+#from collections import Iterable
 # https://blog.csdn.net/ydw_ydw/article/details/82227699
+
+def decode(items):
+	print(items.attrib)
+	for item in items:
+		if isinstance(item, etree._Element):
+			decode(item)
+		if item.text != None and item.text.strip() != '':
+			print(item.tag)
+
+
 def main():
 	text ='''<?xml version="1.0" encoding="utf-8"?>
 		 <item tag='itemtag'>
-			 <cuo></cuo>
-			 <fv><subfv></subfv></fv>
+			 <cuo>c</cuo>
+			 <fv><subfv>sub</subfv></fv>
 			 <data>
-			 <CaseInfo></CaseInfo>
+			 <CaseInfo>abc</CaseInfo>
 			 </data>
 		 </item>
 		'''
 	
 	root = etree.fromstring(text.encode('utf-8')) #解析对象输出代码
 
-	print(etree.tostring(root).decode('utf-8'))
+	decode(root)
+
+	"""
 	print('root.text = %s' % root.text)
 	print('root.tag = %s' % root[1].tag) 
 
 	child = etree.SubElement(root, 'child')
 	child.text = 'childtext'
 	print(etree.tostring(root).decode('utf-8'))
-
+	"""
 
 if __name__ == '__main__':
 	main()
